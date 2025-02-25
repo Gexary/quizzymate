7a155b0ae20df05a3debe5a5890e51ad6a9319fc
Gexary <gexary.gex@gmail.com>
2025-02-25 14:26:07 +0000

    test

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
+  eslint: {
+    dirs: ["src"],
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
