3d8bf45be5d7fbbef446dd60e9498ae16114dce8
Gexary <gexary.gex@gmail.com>
2025-03-23 23:11:59 +0000

    ui update

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -11,10 +11,20 @@ const colors: Record<BackgroundColor, string> = {
   yellow: "from-yellow-500 to-yellow-300",
 };
 
+const patterns: Record<BackgroundColor, string> = {
+  purple: "bg-pattern-1",
+  blue: "bg-pattern-2",
+  red: "",
+  green: "",
+  yellow: "",
+};
+
 export function Background({ children, color = "blue" }: { children: React.ReactNode; color?: BackgroundColor }) {
   return (
     <div className={cn("bg-gradient-to-t h-screen w-full", colors[color])}>
-      <div className="bg-pattern w-full h-full flex flex-col items-center justify-center overflow-y-auto">{children}</div>
+      <div className={`bg-pattern w-full h-full flex flex-col items-center justify-center overflow-y-auto ${patterns[color]}`}>
+        {children}
+      </div>
     </div>
   );
 }
